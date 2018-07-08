921b9aa3d43071577dafe507e6cfc3870da37be1
athimanshu <singh.himanshu86@gmail.com>
2018-07-08 12:07:11 +0530

    commit from IDEA

---
+++ src/main/java/com/springboot/pilot/PilotArtifact/PilotArtifactApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.pilot.PilotArtifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotArtifactApplication.class, args);
+	}
+}
